8f4a8e2cbbd8ca7add0a0cd204133916bbae91f3
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-17 20:17:24 +0530

    added place and cloud

--- weather_app/src/components/weather/temp.js
+++ weather_app/src/components/weather/temp.js
@@ -3,36 +3,43 @@ import "./style.css";
 
 const temp = () => {
   return (
-   <>
-   <div className="wrap">
-    <div className="search">
-    <input 
-    type="text" 
-    placeholder = "Search..."
-    autofocus
-    id="search"
-    className="searchTerm"
-    />
-     <button className="searchButton" type="button">
-        Search
-    </button>
-    </div>
-   </div>
+    <>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="text"
+            placeholder="Search..."
+            autofocus
+            id="search"
+            className="searchTerm"
+          />
+          <button className="searchButton" type="button">
+            Search
+          </button>
+        </div>
+      </div>
 
-  {/*temp card*/} 
-    <article className="widget">
+      {/*temp card*/}
+      <article className="widget">
         <div className="weatherIcon">
-        <i className="wi wi-day-sunny"> </i>
+          <i className="wi wi-day-sunny"> </i>
         </div>
         <div className="weatherInfo">
-            <div className="temperature">
-                <span>25.5&deg;</span>
+          <div className="temperature">
+            <span>25.5&deg;</span>
+          </div>
+          <div className="description">
+            <div className="weatherCondition">
+                Sunny
             </div>
-
+            <div className="place">
+                Nashik, India
+            </div>
+          </div>
         </div>
-    </article>
-   </>
-  )
+      </article>
+    </>
+  );
 }
 
 export default temp
