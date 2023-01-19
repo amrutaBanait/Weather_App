b6cb9445723d67290fdd9835ed86d1bbee1bbc01
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2023-01-19 23:28:52 +0530

    formatting done

--- weather_app/src/components/weather/temp.js
+++ weather_app/src/components/weather/temp.js
@@ -1,45 +1,45 @@
-import React from 'react'
+import React from "react";
 import "./style.css";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import WeatherCard from "./weatherCard";
 
 const Temp = () => {
-const [searchValue, setSearchValue] = useState("Nashik") 
-const [tempInfo, setTempInfo] = useState({});
+  const [searchValue, setSearchValue] = useState("Nashik");
+  const [tempInfo, setTempInfo] = useState({});
 
-const getWeatherInfo = async() =>{
-try{
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=68e6879f536e665674e71142cacb2034`
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=68e6879f536e665674e71142cacb2034`;
 
-    const result = await fetch(url);
-    const data = await result.json();
-    // console.log(data);
+      const result = await fetch(url);
+      const data = await result.json();
+      // console.log(data);
 
-    const {temp,pressure,humidity} = data.main;
-    const { main:weathermood } = data.weather[0]
-    const {name} = data;
-    const {speed} = data.wind
-    const {country, sunset} = data.sys
-   
-    const myNewWeatherInfo = {
-      temp,
-      pressure,
-      humidity,
-      weathermood,
-      name,
-      speed,
-      country,
-      sunset
-    };
-setTempInfo (myNewWeatherInfo)
-}catch(error){
-  console.log(error, "error")
-}
-}
+      const { temp, pressure, humidity } = data.main;
+      const { main: weathermood } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
 
-useEffect (() =>{
-  getWeatherInfo()
-},[])
+      const myNewWeatherInfo = {
+        temp,
+        pressure,
+        humidity,
+        weathermood,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+      setTempInfo(myNewWeatherInfo);
+    } catch (error) {
+      console.log(error, "error");
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  }, []);
   return (
     <>
       <div className="wrap">
@@ -53,18 +53,19 @@ useEffect (() =>{
             value={searchValue}
             onChange={(e) => setSearchValue(e.target.value)}
           />
-          <button 
-          className="searchButton" 
-          type="button"
-          onClick={getWeatherInfo}>
+          <button
+            className="searchButton"
+            type="button"
+            onClick={getWeatherInfo}
+          >
             Search
           </button>
         </div>
       </div>
 
-      <WeatherCard {...tempInfo}/>
+      <WeatherCard {...tempInfo} />
     </>
   );
-}
+};
 
-export default Temp
+export default Temp;
